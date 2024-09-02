714cedd42b61bd69b9b48aee1a2c28f591f74517
Marcos Piriz <marcospiriz1996@gmail.com>
2024-09-02 14:53:54 -0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = AppModule.port || 3001;
+  app.setGlobalPrefix('pormel/api/v1/');
 
-  await app.listen(PORT);
+	const PORT = AppModule.port || 3001;
+
+	await app.listen(PORT, '0.0.0.0');
+	console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
