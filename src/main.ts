72057e0d7e85bbe16dd623ad93f2da7ed1e15535
Marcos Piriz <marcospiriz1996@gmail.com>
2024-09-06 11:17:46 -0300

    Add GHA to publish docker image to AWS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+const pormel = require('@marcospiriztorrez/test-pormel-package');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('pormel/api/v1/');
 
 	const PORT = AppModule.port || 3001;
+
+	pormel();
 
 	await app.listen(PORT, '0.0.0.0');
 	console.log(`Application is running on: ${await app.getUrl()}`);
